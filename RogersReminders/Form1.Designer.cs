3b3cba01f382469dccc1059fe3d499682c82e7e5
Dean Singleton <1256599@stu.tmc.ac.uk>
2018-05-28 20:48:36 +0100

    Edit reminders

--- RogersReminders/Form1.Designer.cs
+++ RogersReminders/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.Submit = new System.Windows.Forms.Button();
             this.ReminderType = new System.Windows.Forms.ComboBox();
@@ -37,17 +38,23 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.ReminderInput = new System.Windows.Forms.RichTextBox();
             this.DatePicker = new System.Windows.Forms.DateTimePicker();
             this.Title = new System.Windows.Forms.TextBox();
             this.ReminderList = new System.Windows.Forms.CheckedListBox();
+            this.EditSelected = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.ReminderInput = new System.Windows.Forms.TextBox();
+            this.EditInput = new System.Windows.Forms.TextBox();
+            this.AddEdited = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.label1.Location = new System.Drawing.Point(405, 9);
+            this.label1.Location = new System.Drawing.Point(327, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(245, 31);
             this.label1.TabIndex = 1;
@@ -56,7 +63,7 @@
             // Submit
             // 
             this.Submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.Submit.Location = new System.Drawing.Point(37, 693);
+            this.Submit.Location = new System.Drawing.Point(12, 404);
             this.Submit.Name = "Submit";
             this.Submit.Size = new System.Drawing.Size(75, 31);
             this.Submit.TabIndex = 4;
@@ -72,7 +79,7 @@
             this.ReminderType.Items.AddRange(new object[] {
             "Reminder",
             "Birthday"});
-            this.ReminderType.Location = new System.Drawing.Point(44, 217);
+            this.ReminderType.Location = new System.Drawing.Point(15, 193);
             this.ReminderType.Name = "ReminderType";
             this.ReminderType.Size = new System.Drawing.Size(134, 26);
             this.ReminderType.TabIndex = 6;
@@ -80,9 +87,9 @@
             // ClearSelected
             // 
             this.ClearSelected.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.ClearSelected.Location = new System.Drawing.Point(474, 652);
+            this.ClearSelected.Location = new System.Drawing.Point(453, 362);
             this.ClearSelected.Name = "ClearSelected";
-            this.ClearSelected.Size = new System.Drawing.Size(87, 31);
+            this.ClearSelected.Size = new System.Drawing.Size(117, 29);
             this.ClearSelected.TabIndex = 7;
             this.ClearSelected.Text = "Clear selected";
             this.ClearSelected.UseVisualStyleBackColor = true;
@@ -91,9 +98,9 @@
             // ClearAll
             // 
             this.ClearAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.ClearAll.Location = new System.Drawing.Point(474, 693);
+            this.ClearAll.Location = new System.Drawing.Point(453, 403);
             this.ClearAll.Name = "ClearAll";
-            this.ClearAll.Size = new System.Drawing.Size(87, 31);
+            this.ClearAll.Size = new System.Drawing.Size(117, 32);
             this.ClearAll.TabIndex = 8;
             this.ClearAll.Text = "Clear all";
             this.ClearAll.UseVisualStyleBackColor = true;
@@ -103,7 +110,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label2.Location = new System.Drawing.Point(37, 112);
+            this.label2.Location = new System.Drawing.Point(6, 93);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(50, 24);
             this.label2.TabIndex = 9;
@@ -113,7 +120,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label5.Location = new System.Drawing.Point(40, 190);
+            this.label5.Location = new System.Drawing.Point(11, 166);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(138, 24);
             this.label5.TabIndex = 15;
@@ -123,7 +130,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label3.Location = new System.Drawing.Point(37, 283);
+            this.label3.Location = new System.Drawing.Point(11, 250);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(109, 24);
             this.label3.TabIndex = 17;
@@ -133,32 +140,24 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label4.Location = new System.Drawing.Point(37, 586);
+            this.label4.Location = new System.Drawing.Point(5, 334);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(132, 24);
             this.label4.TabIndex = 18;
             this.label4.Text = "Completed by:";
             // 
-            // ReminderInput
-            // 
-            this.ReminderInput.Location = new System.Drawing.Point(37, 310);
-            this.ReminderInput.Name = "ReminderInput";
-            this.ReminderInput.Size = new System.Drawing.Size(492, 248);
-            this.ReminderInput.TabIndex = 19;
-            this.ReminderInput.Text = "";
-            // 
             // DatePicker
             // 
-            this.DatePicker.Location = new System.Drawing.Point(44, 623);
+            this.DatePicker.Location = new System.Drawing.Point(12, 371);
             this.DatePicker.Name = "DatePicker";
             this.DatePicker.Size = new System.Drawing.Size(215, 20);
             this.DatePicker.TabIndex = 21;
             // 
             // Title
             // 
-            this.Title.Location = new System.Drawing.Point(43, 137);
+            this.Title.Location = new System.Drawing.Point(12, 118);
             this.Title.Name = "Title";
-            this.Title.Size = new System.Drawing.Size(134, 20);
+            this.Title.Size = new System.Drawing.Size(242, 20);
             this.Title.TabIndex = 22;
             // 
             // ReminderList
@@ -166,21 +165,73 @@
             this.ReminderList.CheckOnClick = true;
             this.ReminderList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.ReminderList.FormattingEnabled = true;
-            this.ReminderList.Location = new System.Drawing.Point(576, 112);
+            this.ReminderList.HorizontalScrollbar = true;
+            this.ReminderList.Location = new System.Drawing.Point(576, 74);
             this.ReminderList.Name = "ReminderList";
-            this.ReminderList.Size = new System.Drawing.Size(488, 613);
+            this.ReminderList.Size = new System.Drawing.Size(309, 361);
             this.ReminderList.TabIndex = 23;
-            this.ReminderList.SelectedIndexChanged += new System.EventHandler(this.ReminderList_SelectedIndexChanged);
+            // 
+            // EditSelected
+            // 
+            this.EditSelected.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.EditSelected.Location = new System.Drawing.Point(330, 362);
+            this.EditSelected.Name = "EditSelected";
+            this.EditSelected.Size = new System.Drawing.Size(117, 29);
+            this.EditSelected.TabIndex = 24;
+            this.EditSelected.Text = "Edit selected";
+            this.EditSelected.UseVisualStyleBackColor = true;
+            this.EditSelected.Click += new System.EventHandler(this.EditSelected_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(161, 29);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            // 
+            // ReminderInput
+            // 
+            this.ReminderInput.Location = new System.Drawing.Point(12, 277);
+            this.ReminderInput.Name = "ReminderInput";
+            this.ReminderInput.Size = new System.Drawing.Size(244, 20);
+            this.ReminderInput.TabIndex = 27;
+            // 
+            // EditInput
+            // 
+            this.EditInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.EditInput.Location = new System.Drawing.Point(330, 336);
+            this.EditInput.Name = "EditInput";
+            this.EditInput.Size = new System.Drawing.Size(242, 24);
+            this.EditInput.TabIndex = 28;
+            // 
+            // AddEdited
+            // 
+            this.AddEdited.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.AddEdited.Location = new System.Drawing.Point(330, 406);
+            this.AddEdited.Name = "AddEdited";
+            this.AddEdited.Size = new System.Drawing.Size(117, 29);
+            this.AddEdited.TabIndex = 29;
+            this.AddEdited.Text = "Add edited";
+            this.AddEdited.UseVisualStyleBackColor = true;
+            this.AddEdited.Click += new System.EventHandler(this.AddEdited_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1076, 737);
+            this.ClientSize = new System.Drawing.Size(891, 443);
+            this.Controls.Add(this.AddEdited);
+            this.Controls.Add(this.EditInput);
+            this.Controls.Add(this.ReminderInput);
+            this.Controls.Add(this.EditSelected);
             this.Controls.Add(this.ReminderList);
             this.Controls.Add(this.Title);
             this.Controls.Add(this.DatePicker);
-            this.Controls.Add(this.ReminderInput);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label5);
@@ -190,9 +241,12 @@
             this.Controls.Add(this.ReminderType);
             this.Controls.Add(this.Submit);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -208,10 +262,15 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.RichTextBox ReminderInput;
         private System.Windows.Forms.DateTimePicker DatePicker;
         private System.Windows.Forms.TextBox Title;
         private System.Windows.Forms.CheckedListBox ReminderList;
+        private System.Windows.Forms.Button EditSelected;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.TextBox ReminderInput;
+        private System.Windows.Forms.TextBox EditInput;
+        private System.Windows.Forms.Button AddEdited;
     }
 }
 
--- RogersReminders/Form1.cs
+++ RogersReminders/Form1.cs
@@ -14,7 +14,10 @@ namespace RogersReminders
 {
     public partial class Form1 : Form
     {
+        // variableto store the path f the file that the reminders will be stored in
         string file = @"C:\Users\Public\reminders\reminders.txt";
+        // variable to store the value of the edit text box
+        string reminderEdit;
 
         public Form1()
         {
@@ -23,156 +26,247 @@ namespace RogersReminders
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // selectt the first item in the drop down 
+            ReminderType.SelectedIndex = 0;
+            // store each line of tthe reminders file as an index in an array
             string[] reminders = File.ReadAllLines(file);
 
+            // iterate over the array
             foreach (string reminder in reminders)
             {
                 if(string.IsNullOrWhiteSpace(reminder))
                 {
-
+                    // dont add the reminder if it doesn't contain a value
                 } else
                 {
+                    // add the reminder to the list of reminders
                     ReminderList.Items.Add(reminder);
                 }
             }
-
-            ReminderType.SelectedIndex = 0;
         }
 
         private void Submit_Click(object sender, EventArgs e)
         {
-            bool loop = false;
-
-            while(!loop)
+            //check each input ad ensure that they have a value
+            if (string.IsNullOrWhiteSpace(ReminderType.Text))
             {
-                if (string.IsNullOrWhiteSpace(ReminderType.Text))
-                {
-                    MessageBox.Show("You must select an item from the list");
-                    return;
-                }
-
-                if (string.IsNullOrWhiteSpace(Title.Text))
-                {
-                    MessageBox.Show("You must give the reminder a title");
-                    return;
-                }
-
-                if (string.IsNullOrWhiteSpace(ReminderInput.Text))
-                {
-                    MessageBox.Show("You must give a description of the reminder");
-                    return;
-                }
-                loop = true;
+                MessageBox.Show("You must select an item from the list");
+                return;
             }
 
+            if (string.IsNullOrWhiteSpace(Title.Text))
+            {
+                MessageBox.Show("You must give the reminder a title");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(ReminderInput.Text))
+            {
+                MessageBox.Show("You must give a description of the reminder");
+                return;
+            }
+
+            //varibles to store the values of the input boxes and dropdown
             string reminderType = ReminderType.Text;
             string title = Title.Text;
             string reminder = ReminderInput.Text;
             string dueDate = DatePicker.Text;
+            //variable to store the reminder to add to a file that is out of the scope of where it is written to a file
             string line = "";
-            string path = @"C:\Users\Public\reminders";
 
-            try
+            try // try to create the directory where the reminder text file will e stored
             {
-                if(Directory.Exists(path))
+                if(Directory.Exists(@"C:\Users\Public\reminders"))
                 {
+                    // if the directory already exists, move on
                 } else
                 {
-                    DirectoryInfo di = Directory.CreateDirectory(path);
+                    //create the directory
+                    DirectoryInfo di = Directory.CreateDirectory(@"C:\Users\Public\reminders");
                 }
 
             } catch (SystemException)
             {
-                MessageBox.Show("Something went wrong when creating the directory"); // about as useful as all other windows errors
+                // if there was an error while creating the directory show a message box
+                MessageBox.Show("Something went wrong when creating the directory");
             }
 
             if (reminderType == "Reminder")
             {
+                //create an new instance of te class "GenericReminder"
                 var newReminder = new GenericReminder(title, reminder, dueDate);
+                // create the reminder that will be added to the text file and the list where the reminders will be stored
                 line = newReminder.title + ": " + newReminder.reminder + " - " + newReminder.completedBy + Environment.NewLine;
+                // add the reminder to the list
                 ReminderList.Items.Add(line);
 
-            }
-            else if(reminderType == "Birthday")
+            }else if(reminderType == "Birthday")
             {
+                //create an new instance of te class "BirthdayReminder"
                 var newReminder = new BirthdayReminder(title, reminder, dueDate);
+                // create the reminder that will be added to the text file and the list where the reminders will be stored
                 line = newReminder.title + ": " + newReminder.reminder + " - " + newReminder.birthday + Environment.NewLine;
+                // add the reminder to the list
                 ReminderList.Items.Add(line);
             }
 
             try
             {
+                // try to add the reminder to the text file
                 File.AppendAllText(file, line);
             }
             catch (SystemException)
             {
+                // if there was an error 
                 MessageBox.Show("Could not write to file");
             }
+
+            // reset the input boxes for the title and reminder description
+            Title.Text = "";
+            ReminderInput.Text = "";
         }
 
         private void ClearSelected_Click(object sender, EventArgs e)
         {
-            var remindersArr = File.ReadAllLines(file);
+            // create an array with each line of the reminders file as an array index
+            string[] remindersArr = File.ReadAllLines(file);
+            // delete the file because remaining reminders will be appended to the file and not overwritten
             File.Delete(file);
 
-            var reminders = remindersArr.ToList();
 
-            List<string> toRemove = new List<string>();
+            // create a list that will contain reminders to keep
+            List<string> toKeep = new List<string>();
 
             foreach(object reminder in ReminderList.Items)
             {
+                // if the reminder in the reminders list isn't checked
                 if(!ReminderList.CheckedItems.Contains(reminder))
                 {
-                    toRemove.Add(reminder.ToString());
+                    // add it to the list of reminders to keep
+                    toKeep.Add(reminder.ToString());
+                    // create te file if it doesnt exist and append the reminder to it
                     File.AppendAllText(file, reminder.ToString() + Environment.NewLine);
                 }
             }
 
+            //clear all items in the list
             ReminderList.Items.Clear();
 
-            string[] newReminderArr = File.ReadAllLines(file);
-
-            foreach(string reminder in newReminderArr)
+            if (toKeep.Count == 0)
             {
-                if (string.IsNullOrWhiteSpace(reminder))
-                {
+                //if there are no reminders to keep, do nothing
+                return;
+            } else
+            {
+                // get each line in the reminders file and add it to an index in the array
+                string[] newReminderArr = File.ReadAllLines(file);
 
-                } else
+                foreach (string reminder in newReminderArr)
                 {
-                    ReminderList.Items.Add(reminder);
+                    //if the reminder in the array isn't empty add it to the 
+                    if (string.IsNullOrWhiteSpace(reminder))
+                    {
+
+                    }
+                    else
+                    {
+                        ReminderList.Items.Add(reminder);
+                    }
                 }
             }
         }
 
         private void ClearAll_Click(object sender, EventArgs e)
         {
-            File.Delete(file);
-            ReminderList.Items.Clear();
+            // if the reminder list doesnt have any reminders do nothing
+            if(ReminderList.Items.Count == 0)
+            {
+                return;
+            }
+
+            //show a dialog box with the options "Yes" and "No"
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to delete all reminders?", "Delete all", MessageBoxButtons.YesNo);
+
+            //if the user clicks the "Yes" option
+            if (dialogResult == DialogResult.Yes)
+            {
+                //delete the file
+                File.Delete(file);
+                // clear the reminder list
+                ReminderList.Items.Clear();
+            } else if (dialogResult == DialogResult.No)
+            {
+                return;
+            } 
         }
 
-        private void ReminderList_SelectedIndexChanged(object sender, EventArgs e)
+        private void EditSelected_Click(object sender, EventArgs e)
         {
+            // get the total number of checked items and store it as a varibale
+            int checkedCounter = ReminderList.CheckedItems.Count;
+
+            //alert the user that they have to select a reminder to edit it
+            if (ReminderList.SelectedIndex < 0)
+            {
+                MessageBox.Show("You must have an item selected before you can edit it");
+                return;
+            } else if(checkedCounter > 1)
+            {
+                // alert the user that they can't edit more than 1 reminder at a time
+                MessageBox.Show("You can only edit one reminder at a time");
+                return;
+            }
+
+            // get the selected reminder and store it as a variable
+            string currentVal = ReminderList.Items[ReminderList.SelectedIndex].ToString();
+            // add the selected reminder to the edit box;
+            EditInput.Text = currentVal;
+            //set the selected reminder as the "reminderEdit" variable
+            reminderEdit = currentVal;
+        }
+
+        private void AddEdited_Click(object sender, EventArgs e)
+        {
+            // check if the edit box is empty
+            if (String.IsNullOrWhiteSpace(EditInput.Text))
+            {
+                // alert the user that the edit box must contain a value
+                MessageBox.Show("The edited reminder must contain a value");
+                return;
+            }
+
+            // remove the selected reminder from the reminder list
+            ReminderList.Items.Remove(reminderEdit);
+            // add the edited reminder to the reminder list
+            ReminderList.Items.Add(EditInput.Text);
+            // remove the contents of the edit box
+            EditInput.Text = "";
         }
     }
 
     class Reminder
     {
+        // variables that are shared across all types of reminder
         public string title;
         public string reminder;
     }
 
+    // inherit the variables from the base reminder class
     class GenericReminder : Reminder
     {
+        // create a variable unique to this class
         public string completedBy;
 
         public GenericReminder(string _title, string _reminder, string _completedBy)
         {
+            //set the values of the variables to the users input
             this.title = _title;
             this.reminder = _reminder;
             completedBy = _completedBy;
         }
     }
 
+    // class that adds "Birthday" to the start of the title as a form of tag for the reminder
     class BirthdayReminder : Reminder
     {
         public string birthday;
